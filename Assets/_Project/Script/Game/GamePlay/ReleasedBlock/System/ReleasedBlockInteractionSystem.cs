b48f220e4fd38f8eed835561426fdbb0f5a6e76a
TunaPC <tuananh@hadesgame.studio>
2026-07-22 19:18:32 +0700

    Revert "tối ưu performance"
    This reverts commit 828dd5d1401d87979873b70ef18fe9a9296252ea.

--- Assets/_Project/Script/Game/GamePlay/ReleasedBlock/System/ReleasedBlockInteractionSystem.cs
+++ Assets/_Project/Script/Game/GamePlay/ReleasedBlock/System/ReleasedBlockInteractionSystem.cs
@@ -9,7 +9,6 @@ using Unity.Transforms;
 public partial struct ReleasedBlockInteractionSystem : ISystem
 {
     private EntityQuery _query;
-    private EntityQuery _motionQuery;
     private EntityQuery _suctionTransitQuery;
     private NativeReference<int> _suckedCount;
     private JobHandle _lastInteractionHandle;
@@ -25,13 +24,6 @@ public partial struct ReleasedBlockInteractionSystem : ISystem
             .WithAllRW<SuctionTransit>()
             .WithAllRW<Simulate>()
             .WithAllRW<LocalTransform>()
-            .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)
-            .Build();
-        _motionQuery = SystemAPI.QueryBuilder()
-            .WithAllRW<LocalTransform>()
-            .WithAllRW<PhysicsVelocity>()
-            .WithAll<ReleasedBlockComponent>()
-            .WithAll<SuctionTransit>()
             .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)
             .Build();
         _suctionTransitQuery = SystemAPI.QueryBuilder()
@@ -103,7 +95,7 @@ public partial struct ReleasedBlockInteractionSystem : ISystem
                 continue;
             }
             if (spawner.TryCreatePendingSawPushJob(out TextureBlockSpawner.SawPushJob sawPushJob))
-                dependency = sawPushJob.ScheduleParallel(_motionQuery, dependency);
+                dependency = sawPushJob.ScheduleParallel(_query, dependency);
         }
 
         for (int i = TextureBlockSpawner.ActiveSpawnerCount - 1; i >= 0; i--)
@@ -113,7 +105,7 @@ public partial struct ReleasedBlockInteractionSystem : ISystem
                 spawner.TryCreateSolidConstraintJob(
                     out TextureBlockSpawner.ReleasedBlockSolidConstraintJob solidConstraintJob))
             {
-                dependency = solidConstraintJob.ScheduleParallel(_motionQuery, dependency);
+                dependency = solidConstraintJob.ScheduleParallel(_query, dependency);
             }
         }
         state.Dependency = dependency;
--- Assets/_Project/Script/Game/GamePlay/ReleasedBlock/System/ReleasedBlockPlanarConstraintSystem.cs
+++ Assets/_Project/Script/Game/GamePlay/ReleasedBlock/System/ReleasedBlockPlanarConstraintSystem.cs
@@ -1,6 +1,5 @@
 using Unity.Burst;
 using Unity.Entities;
-using Unity.Jobs;
 using Unity.Physics;
 using Unity.Physics.Systems;
 using Unity.Transforms;
@@ -10,18 +9,16 @@ using Unity.Transforms;
 public partial struct ReleasedBlockPlanarConstraintSystem : ISystem
 {
     private EntityQuery _query;
-    private EntityQuery _suctionQuery;
 
     public void OnCreate(ref SystemState state)
     {
         _query = SystemAPI.QueryBuilder()
             .WithAllRW<LocalTransform>()
             .WithAllRW<PhysicsVelocity>()
-            .WithAll<ReleasedBlockComponent>()
-            .Build();
-        _suctionQuery = SystemAPI.QueryBuilder()
             .WithAllRW<PhysicsGravityFactor>()
-            .WithAll<SuctionTransit>()
+            .WithAllRW<ReleasedBlockComponent>()
+            .WithAllRW<Simulate>()
+            .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState)
             .Build();
         state.RequireForUpdate(_query);
     }
@@ -29,7 +26,6 @@ public partial struct ReleasedBlockPlanarConstraintSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        JobHandle planarHandle = new ReleasedBlockPlanarConstraintJob().ScheduleParallel(_query, state.Dependency);
-        state.Dependency = new ReleasedBlockSuctionGravityResetJob().ScheduleParallel(_suctionQuery, planarHandle);
+        state.Dependency = new ReleasedBlockPlanarConstraintJob().ScheduleParallel(_query, state.Dependency);
     }
 }
